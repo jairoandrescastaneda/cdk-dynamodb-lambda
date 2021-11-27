f5983d8ae5caf1d797d0164b85064aabb22cb7bb
jairoufps <jairoandrescp@ufps.edu.co>
2021-11-26 23:52:48 -0500

    Initial commit

---
+++ tests/unit/test_myproject_stack.py
@@ -0,0 +1,19 @@
+# from aws_cdk import (
+#         core,
+#         assertions
+#     )
+
+# from myproject.myproject_stack import MyprojectStack
+
+
+# example tests. To run these tests, uncomment this file along with the example
+# resource in myproject/myproject_stack.py
+def test_sqs_queue_created():
+#     app = core.App()
+#     stack = MyprojectStack(app, "myproject")
+#     template = assertions.Template.from_stack(stack)
+
+#     template.has_resource_properties("AWS::SQS::Queue", {
+#         "VisibilityTimeout": 300
+#     })
+  pass
